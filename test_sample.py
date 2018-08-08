00c3965712a4cc31bce1b305a9c8705e2bbe5d19
Timothée GERMAIN <timothee@lumapps.com>
2018-08-08 11:21:11 +0200

    test workflow with black and flake8

--- test_sample.py
+++ test_sample.py
@@ -2,8 +2,17 @@ def inc(x):
     return x + 1
 
 
-#def test_failing():
+# def test_failing():
 #    assert inc(3) == 5
+
 
 def test_ok():
     assert inc(3) == 4
+
+
+# following list is not black-compatible
+my_list = [
+    1,
+    2,
+    3,
+]
